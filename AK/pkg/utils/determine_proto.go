20b73a271704cc7e939449158c7f28911909667a
ismael FALL <ismael.fall@epitech.eu>
2022-10-06 16:39:12 +0200

    fix: remove debug
    Signed-off-by: ismael FALL <ismael.fall@epitech.eu>

--- AK/pkg/utils/determine_proto.go
+++ AK/pkg/utils/determine_proto.go
@@ -1,14 +1,11 @@
 package utils
 
 import (
-	"fmt"
-
 	pb "github.com/golang/protobuf/proto"
 )
 
 func IsProtoType(b []byte, t pb.Message) pb.Message {
 	// TODO: try to make this with proto.reflect
-	fmt.Println(string(b))
 	if pb.Unmarshal(b, t) != nil {
 		return nil
 	}
